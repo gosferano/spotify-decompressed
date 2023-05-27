ceb22161754ecf8d075ed38a8d8e70adf1392666
Gytis Barkauskas <gytisbark@gmail.com>
2023-05-27 16:26:15 +0300

    Added dark theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,15 +6,26 @@
       aria-label="main navigation"
     >
       <level>
-        <span class="level-item">
-          <h1 class="is-size-4 has-text-weight-semibold">
-            <span class="icon is-small"
-              ><i class="fas fa-image" aria-hidden="true"></i></span
-            >Spotify Decompressed
-          </h1>
-        </span>
+        <h1 class="level-item is-size-4 has-text-weight-semibold">
+          <span class="icon is-small"
+            ><i class="fas fa-image" aria-hidden="true"></i></span
+          >Spotify Decompressed
+        </h1>
+        <o-field class="level-item">
+          <o-switch
+            v-model="colorMode.preference"
+            true-value="dark"
+            false-value="light"
+          >
+            {{ colorMode.preference }}
+          </o-switch>
+        </o-field>
       </level>
     </nav>
     <slot />
   </span>
 </template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+</script>
