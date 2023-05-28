445ec9dddf06280e267c0df654e6087f07b67b7c
Gytis Barkauskas <gytisbark@gmail.com>
2023-05-28 14:03:23 +0300

    Made navbar smaller

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,33 @@
 <template>
   <span>
     <nav
-      class="header is-vcentered"
+      class="navbar is-transparent"
       role="navigation"
       aria-label="main navigation"
     >
-      <level>
+      <div class="navbar-brand">
         <h1 class="level-item is-size-4 has-text-weight-semibold">
           <span class="icon is-small"
             ><i class="fas fa-image" aria-hidden="true"></i></span
           >Spotify Decompressed
         </h1>
-        <o-field class="level-item">
-          <o-switch
-            v-model="colorMode.preference"
-            true-value="dark"
-            false-value="light"
-          >
-            {{ colorMode.preference }}
-          </o-switch>
-        </o-field>
-      </level>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <o-field>
+              <o-switch
+                v-model="colorMode.preference"
+                true-value="dark"
+                false-value="light"
+              >
+                {{ colorMode.preference }}
+              </o-switch>
+            </o-field>
+          </div>
+        </div>
+      </div>
     </nav>
 
     <span id="main-content">
