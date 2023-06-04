fd283f3aa02bc5abda89a0664bae27e5e1c89082
Gytis Barkauskas <gytisbark@gmail.com>
2023-06-04 22:12:53 +0300

    Converted Spotify log in and log out buttons to components

---
+++ components/SpotifyLogInButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <o-button
+    tag="a"
+    :href="useSpotifyUserStore().authorizeUrl"
+    target="blank"
+    icon-right="spotify"
+    class="is-primary"
+    :disabled="!$isDevEnvironment()"
+  >
+    Log in with
+  </o-button>
+</template>
+
+<script setup lang="ts">
+import { useSpotifyUserStore } from '~/stores/spotifyUserStore'
+</script>
