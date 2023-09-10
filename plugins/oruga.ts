b52b99b714f2d06c479157a57a2952973ce3a3cd
Gytis Barkauskas <gytisbark@gmail.com>
2023-09-10 21:46:45 +0300

    Updated code to match eslint code style

--- plugins/oruga.ts
+++ plugins/oruga.ts
@@ -1,6 +1,6 @@
-import { defineNuxtPlugin } from '#app'
 import Oruga from '@oruga-ui/oruga-next'
 import { bulmaConfig } from '@oruga-ui/theme-bulma'
+import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin(({ vueApp }) => {
   vueApp.use(Oruga, bulmaConfig) // .mount('#app');
